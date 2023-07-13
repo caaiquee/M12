3411034a16fe2018949332e2665347070f1711de
caaiquee <05caique@gmail.com>
2023-07-13 03:28:12 -0300

    Finalizando o teste

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -1,4 +1,8 @@
 /// <reference types="cypress"/>
+const perfil = require('../fixtures/perfil.json')
+const dados = require('../fixtures/dados.json')
+import CheckoutPage from '../support/page_objects/nome-funcionliada.page';
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -10,11 +14,27 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('')
+        cy.get('.icon-user-unfollow').click()
+        cy.login(perfil.usuario, perfil.senha)
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
         //TODO 
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.get('[class="product-block grid"]').eq(0).click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(4)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 4)
+        cy.get('.dropdown-toggle > .mini-cart-items').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+        CheckoutPage.preencherCheckoutPage(dados.nome, dados.sobrenome, dados.empresa, dados.pais, dados.endereco, dados.numero, dados.cidade, dados.estado, dados.cep, dados.telefone, dados.email)
+        cy.get('#payment_method_cod').click()
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+        cy.get('.woocommerce-notice').should('contain', 'Seu pedido foi recebido')
     });
 
 
--- cypress/support/page_objects/nome-funcionliada.page.js
+++ cypress/support/page_objects/nome-funcionliada.page.js
@@ -1,9 +1,22 @@
-class NomeClasse {
+class CheckoutPage {
 
-    nomeMetodo(parametros ){
+    preencherCheckoutPage(nome, sobrenome, empresa, pais, endereco, numero, cidade, estado, cep, telefone, email){
         //ações do método
+        cy.get('#billing_first_name').clear().type(nome)
+        cy.get('#billing_last_name').clear().type(sobrenome)
+        cy.get('#billing_company').clear().type(empresa)
+        cy.get('#select2-billing_country-container').click()
+        cy.get('.select2-search__field').clear().type(pais + '{enter}')
+        cy.get('#billing_address_1').clear().type(endereco)
+        cy.get('#billing_address_2').clear().type(numero)
+        cy.get('#billing_city').clear().type(cidade)
+        cy.get('#select2-billing_state-container').click()
+        cy.get('.select2-search__field').clear().type(estado + '{enter}')
+        cy.get('#billing_postcode').clear().type(cep)
+        cy.get('#billing_phone').clear().type(telefone)
+        cy.get('#billing_email').clear().type(email)
     }
 
 }
 
-export default new NomeClasse()
+export default new CheckoutPage()
